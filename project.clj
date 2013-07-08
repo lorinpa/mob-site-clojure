7036b52792a275a5290809627c0e13ec08439777
Lorin Klugman <lorin@public-action.org>
2013-07-08 18:12:50 -0500

    Updated the license section to GPL

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject mob-site "Rel-1.0"
   :description "Clojure implementation of our mobile generation project"
   :url "http://public-actiion.org"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU General Public License, version 2"
+            :url "http://www.gnu.org/licenses/gpl-2.0.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
     [commons-io/commons-io "2.4"]
   ]
